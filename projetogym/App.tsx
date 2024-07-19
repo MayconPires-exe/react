8c03041a617094b09305ced65e319cf78699fec1
Maycon Pires <piresmaycon4@gmail.com>
2024-07-18 21:57:38 -0300

    Invit inteface user

--- projetogym/App.tsx
+++ projetogym/App.tsx
@@ -3,13 +3,15 @@ import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { THEME } from './src/theme'; 
+
+import { SiginIn } from '@screens/SignIn';
+
 import { Loading } from '@components/Loading';
-import { SiginIn } from '@screens/Signin';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
